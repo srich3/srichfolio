fddc4dc8ffdf3a499c0c0f928b2af594f52f5677
srich3 <spencer.richardson25@gmail.com>
2025-06-16 17:01:11 -0700

    testing home page changes and vercel updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h4>Spencer Richardson</h4>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Click me! {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Hi Sarah!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
